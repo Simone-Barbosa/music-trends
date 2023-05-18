94f3e041868554eb9a0dc3ffaae3942eff3e3a44
Simone Barbosa <simone_barbosa@icloud.com>
2023-05-18 17:53:28 -0300

    melhoria da estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,49 @@
 import { useState } from "react";
 import CardBand from "./components/CardBand";
-import "./styles.css"
+import "./styles.css";
 
 function App() {
-  // const [likes, setLikes] = useState<number>(0);
+  const [likes, setLikes] = useState<number>(0);
 
   return (
-    <div id="orange">
-      <h1 id="title">Music Trends</h1>
+    <>
+      <header>
+        <h1 id="title">Music Trends</h1>
+      </header>
 
-      <div id="purple">
-        <CardBand name="Deep Purple" genres={["Rock Clássico", "Hard Rock"]} />
+      <section>
+        {/* <div className="main"> */}
+
+        <div className="box1" id="purple">
+          <CardBand
+            name="Deep Purple"
+            genres={["Rock Clássico", "Hard Rock"]}
+          />
+        </div>
+
+        <div className="box2" id="red">
+          <CardBand name="Péricles" genres={["Pagode", "Samba"]} />
+        </div>
+
+        <div className="box3" id="green">
+          <CardBand name="Legião Urbana" genres={["Rock", "MPB"]} />
+        </div>
+
+        {/* </div> */}
+      </section>
+
+      <div>
+        <p>Curtidas: {likes}</p>
+        <button
+          onClick={() => {
+            setLikes(likes + 1);
+          }}
+        >
+          Curtir
+        </button>
       </div>
-
-      <div id="red">
-        <CardBand name="Péricles" genres={["Pagode", "Samba"]} />
-      </div>
-
-      <div id="green">
-        <CardBand name="Legião Urbana" genres={["Rock", "MPB"]} />
-      </div>
-
-    </div>
+    </>
   );
 }
 
 export default App;
-
-{/* <div>
-<span>================================</span>
-<p>Curtidas: {likes}</p>
-<button
-  onClick={() => {
-    setLikes(likes + 1);
-  }}
->
-  Curtir
-</button>
-</div> */}
