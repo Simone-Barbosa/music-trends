58acb183ad24d9bcac2bc648a41336eb6b34d7c6
Simone Barbosa <simonebarbosa@ibm.com>
2023-11-13 18:13:07 -0300

    create component GenreSection

---
+++ src/components/GenreSection/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../axios";
+import { getStyleMode } from "../../shared/colors";
+import CardBand from "../CardBand";
+import { usePreferencesUser } from "../../context/userPreferences.context";
+
+interface Artist {
+    id: string;
+    name: string;
+    genres: string[];
+    backgroundColor?: string;
+    images: {
+        height: number;
+        url: string;
+        width: number;
+    }[];
+}
+
+interface GenreSectionProps {
+    genre: string;
+    title: string;
+}
+
+export default function GenreSection({genre,title}:GenreSectionProps) {
+    const [artists, setArtists] = useState<Artist[]>([]);
+    const navigate = useNavigate();
+    //genre composto: power%20metal / hard%20rock
+    async function getArtists() {
+        const response = await axiosInstance.get(`search?q=genre%3A${genre}&type=artist&market=BR&limit=25&offset=0`);
+        setArtists(response.data.artists.items);
+    }
+
+    useEffect(() => {
+        getArtists();
+    }, []);
+
+    const clickCardBand = (artistID: string) => {
+        navigate(`/artist/${encodeURIComponent(artistID)}`);
+    };
+
+    const { darkMode } = usePreferencesUser();
+
+    return (
+        <>
+            {title}
+            <section
+                className="cardBandsSection"
+                style={{
+                    background: getStyleMode(darkMode, 'background_music_trends'),
+                    color: getStyleMode(darkMode, 'font_color_music_trends'),
+                }}
+            >
+                {artists.slice(0,5).map((artist) => {
+                    return <CardBand key={artist.id} name={artist.name} genres={artist.genres} backgroundImage={artist.images[1].url} onClick={() => clickCardBand(artist.id)} />;
+                })}
+            </section>
+            ;
+        </>
+    );
+}
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,59 +1,16 @@
-import { useEffect, useState } from 'react';
-import CardBand from '../CardBand';
 import './home-page.css';
-import { useNavigate } from 'react-router-dom';
-import { usePreferencesUser } from '../../context/userPreferences.context';
-import { getStyleMode } from '../../shared/colors';
-import { axiosInstance } from '../../axios';
-
-interface Artist {
-    id: string;
-    name: string;
-    genres: string[];
-    backgroundColor?: string;
-    images: {
-        height: number;
-        url: string;
-        width: number;
-    }[];
-}
+import GenreSection from '../GenreSection';
 
 export default function HomePage() {
-    const [artists, setArtists] = useState<Artist[]>([]);
-    const navigate = useNavigate();
-    //genre composto: power%20metal / hard%20rock
-    async function getArtists() {
-        const response = await axiosInstance.get('search?q=genre%3Apop&type=artist&market=BR&limit=25&offset=0');
-        setArtists(response.data.artists.items);
-    }
-
-    useEffect(() => {
-        getArtists();
-    }, []);
-
-    const clickCardBand = (artistID: string) => {
-        navigate(`/artist/${encodeURIComponent(artistID)}`);
-    };
-
-    const { darkMode } = usePreferencesUser();
-
     return (
         <>
             <div className="homePageStart">
-                <h3>Music Trends</h3>
-                <h4> Here we will have the Music Trends Introduction </h4>
+                <p>Welcome to the Music Trends Site!</p>
+                <p> Here you will see the most popular musics and artists of the moment! </p>
             </div>
-            <section
-                className="cardBandsSection"
-                style={{
-                    background: getStyleMode(darkMode, 'background_music_trends'),
-                    color: getStyleMode(darkMode, 'font_color_music_trends'),
-                }}
-            >
-                {artists.map((artist) => {
-                    return <CardBand key={artist.id} name={artist.name} genres={artist.genres} backgroundImage={artist.images[1].url} onClick={() => clickCardBand(artist.id)} />;
-                })}
-            </section>
+
+            <GenreSection genre="pop" title="Genero Pop" />
+            <GenreSection genre="rock" title="Genero Rock" />
         </>
     );
 }
